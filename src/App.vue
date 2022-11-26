e32a3a0f5e5fe3929ad2215325777036d755528a
Reniki <hello@reniki.com>
2022-11-26 19:54:44 +0100

    component (script setup)

--- src/App.vue
+++ src/App.vue
@@ -1,53 +1,57 @@
-<script setup>
-import HelloWorld from "./components/HelloWorld.vue";
-import TheWelcome from "./components/TheWelcome.vue";
-</script>
-
 <template>
-  <header>
-    <img
-      alt="Vue logo"
-      class="logo"
-      src="./assets/logo.svg"
-      width="125"
-      height="125"
-    />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+    <ul >
+        <LearningResource
+            v-for="res in storedResources"
+            :key="res.id"
+            :title="res.title"
+            :description="res.description"
+            :link="res.link"
+        />
+    </ul>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
+<script setup>
+import LearningResource from "./components/LearningResources/LearningResource.vue";
+
+const storedResources = [
+    {
+        id: 'official-guide',
+        title: 'Official Guide',
+        description: 'The official Vue.js documentation.',
+        link: 'https://vuejs.org'
+    },
+    {
+        id: 'google',
+        title: 'Google',
+        description: 'Learn to google...',
+        link: 'https://google.com'
+    },
+];
+
+</script>
+
+<!-- <script>
+import LearningResource from "./components/LearningResources/LearningResource.vue";
+
+export default {
+    components: { LearningResource },
+    data() {
+        return {
+            storedResources: [
+                {
+                    id: 'official-guide',
+                    title: 'Official Guide',
+                    description: 'The official Vue.js documentation.',
+                    link: 'https://vuejs.org'
+                },
+                {
+                    id: 'google',
+                    title: 'Google',
+                    description: 'Learn to google...',
+                    link: 'https://google.com'
+                },
+            ],
+        };
+    }
 }
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
+</script> -->
---
+++ src/components/LearningResources/LearningResource.vue
@@ -0,0 +1,31 @@
+<template>
+    <li>
+        <div>
+            <header>
+                <h3>{{ title }}</h3>
+                <button>Delete</button>
+            </header>
+        </div>
+        <p>{{ description }}</p>
+        <nav>
+            <a href="{{ link }}">View Resource</a>
+            <a :href="link">View Resource</a>
+        </nav>
+    </li>
+</template>
+
+<script setup>
+
+defineProps({
+    title: String,
+    description: String,
+    link: String
+})
+
+</script>
+
+<!-- <script>
+export default {
+    props: ['title', 'description', 'link']
+}
+</script> -->
